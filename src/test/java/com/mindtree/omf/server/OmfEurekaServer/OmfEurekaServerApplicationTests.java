df6b831e1892846c87caf7ed03e4d685e98a61c8
Ravi Ranjan Kumar <m1034118@mindtree.com>
2020-09-27 03:13:43 +0530

    Eureka Server code comiiteg

---
+++ src/test/java/com/mindtree/omf/server/OmfEurekaServer/OmfEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.omf.server.OmfEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmfEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
